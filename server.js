ec5a062888158571ec465e9516c114d173da44cb
Gauri Bhand <htcgauribhand@gmail.com>
2022-07-15 21:55:02 +0530

    mongo

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const bodyparser = require("body-parser");
+const path = require('path');
 
 const app = express();
 
@@ -8,6 +10,15 @@ dotenv.config( { path : 'config.env'} )
 const PORT = process.env.PORT || 8080
 
 app.use(morgan('tiny'));
+
+app.use(bodyparser.urlencoded({ extended : true}))
+
+app.set("view engine", "ejs")
+
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
+
 
 app.get('/', (req, res) => {
     res.send("CRUD application")
