c4bc6f843033c325c7062fccc34905b74e6fd452
Gauri Bhand <htcgauribhand@gmail.com>
2022-07-17 11:54:28 +0530

    mongo

--- server.js
+++ server.js
@@ -19,12 +19,10 @@ app.use(bodyparser.urlencoded({ extended : true}))
 
 app.set("view engine", "ejs")
 
-// load assets
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
-// load routers
 app.use('/', require('./server/routes/router'))
 
 app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
