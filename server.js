c1c46002cc8bf1e4b0ef1ddf20e23849de9186e4
Gauri Bhand <htcgauribhand@gmail.com>
2022-07-17 11:53:39 +0530

    monho

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const express = require("express");
+const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyparser = require("body-parser");
 const path = require('path');
+
+const connectDB = require('./server/database/connection');
+
 const app = express();
 
 dotenv.config( { path : 'config.env'} )
@@ -10,14 +13,18 @@ const PORT = process.env.PORT || 8080
 
 app.use(morgan('tiny'));
 
+connectDB();
+
 app.use(bodyparser.urlencoded({ extended : true}))
 
 app.set("view engine", "ejs")
 
+// load assets
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
+// load routers
 app.use('/', require('./server/routes/router'))
 
 app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
--- server/controller/model/database/routes/services/render.js
+++ server/controller/model/database/routes/services/render.js
@@ -2,16 +2,13 @@ const axios = require('axios');
 
 
 exports.homeRoutes = (req, res) => {
-    // Make a get request to /api/users
     axios.get('http://localhost:3000/api/users')
         .then(function(response){
             res.render('index', { users : response.data });
         })
         .catch(err =>{
             res.send(err);
-        })
-
-    
+        })   
 }
 
 exports.add_user = (req, res) =>{
