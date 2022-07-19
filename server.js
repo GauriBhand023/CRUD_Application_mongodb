bd85faf03a5bf9b96644a99c193aba18d9c1f8c5
Gauri Bhand <91608011+GauriBhand023@users.noreply.github.com>
2022-07-19 13:53:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyparser = require("body-parser");
 const path = require('path');
-
 const connectDB = require('./server/database/connection');
 
 const app = express();
