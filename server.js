6928c071bf4a554780dfd7f49aa7c88f44d2e196
Gauri Bhand <htcgauribhand@gmail.com>
2022-07-20 19:14:18 +0530

    mongo

--- server.js
+++ server.js
@@ -11,18 +11,25 @@ const app = express();
 dotenv.config( { path : 'config.env'} )
 const PORT = process.env.PORT || 8080
 
+// log requests
 app.use(morgan('tiny'));
 
+// mongodb connection
 connectDB();
 
+// parse request to body-parser
 app.use(bodyparser.urlencoded({ extended : true}))
 
+// set view engine
 app.set("view engine", "ejs")
+//app.set("views", path.resolve(__dirname, "views/ejs"))
 
+// load assets
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
+// load routers
 app.use('/', require('./server/routes/router'))
 
 app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
--- server/database/connection.js
+++ server/database/connection.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
+
 const connectDB = async () => {
     try{
+        // mongodb connection string
         const con = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
