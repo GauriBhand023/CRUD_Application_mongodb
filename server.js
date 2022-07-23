4b29d5b02ad1bd2d4fc454a0af08e27975a85034
Gauri Bhand <91608011+GauriBhand023@users.noreply.github.com>
2022-07-23 19:38:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,19 +16,14 @@ app.use(morgan('tiny'));
 
 connectDB();
 
-// parse request to body-parser
 app.use(bodyparser.urlencoded({ extended : true}))
 
-// set view engine
 app.set("view engine", "ejs")
-//app.set("views", path.resolve(__dirname, "views/ejs"))
 
-// load assets
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
-// load routers
 app.use('/', require('./server/routes/router'))
 
 app.listen(PORT, ()=> { 
